2d85870560e5dd3f76b48e4dea3f2f1b7b5fbe2a
estherm <esther_mun@hotmail.com>
2015-07-17 12:56:11 -0400

    Revert 3fd05c1..e54b247
    This rolls back to commit 3fd05c1f7a365805912cea086deec66ec3553b39.

--- app/models/post.rb
+++
@@ -1,4 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :user
-  has_attached_file :picture, :style {:medium => "300x300>", :thumb => "150x150>"}, :default_url => "images/:style/missing.png"
-end
